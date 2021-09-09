e995bb33bb2e8fda611006f4d960b92b42fa90d6
huubao123 <61931066+huubao123@users.noreply.github.com>
2021-09-09 20:59:10 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -3,7 +3,7 @@ const textConfig = {
   text2: "Anh có điều này muốn hỏi em nhớ phải trả lời thật lòng nhaaa.",
   text3: "Emyêu anh có phải không nào ._.",
   text4: "Nếu em ko trả lời mà thoát ra tức là muốn làm vợ anh rùi đó nha :v",
-  text5: "em mơ à???",
+  text5: "mơ à???",
   text6: "Yêu ơi là yêu <3",
   text7: "lí do em thích anh đi :vvvv",
   text8: "Gửi cho anh <3",
